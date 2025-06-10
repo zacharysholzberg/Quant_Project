11fe7bf6d1eccc8765d9c570a9de046130096d0d
“Zachary <zacharysholzberg@gmail.com>
2025-06-10 18:00:19 -0400

    data_loader.py finished

---
+++ src/data_loader.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import requests
+import os
+
+class AlphaVantageDataLoader:
+    def __init__(self, api_key: str, symbol: str, outputsize: str = "full"):
+        self.api_key = api_key
+        self.symbol = symbol
+        self.outputsize = outputsize
+
+    def _build_url(self) -> str:
+        return (
+            f"https://www.alphavantage.co/query?"
+            f"function=TIME_SERIES_DAILY"
+            f"&symbol={self.symbol}"
+            f"&outputsize={self.outputsize}"
+            f"&apikey={self.api_key}"
+        )
+
+    def fetch_data(self) -> pd.DataFrame:
+        url = self._build_url()
+        response = requests.get(url)
+        data = response.json()
+
+        if "Time Series (Daily)" not in data:
+            raise ValueError(f"Unexpected response: {data}")
+
+        ts_data = data["Time Series (Daily)"]
+        df = pd.DataFrame.from_dict(ts_data, orient="index")
+        df.columns = [col.split(". ")[1] for col in df.columns]  # clean columns
+        df.index = pd.to_datetime(df.index)
+        df = df.sort_index().astype(float)
+        return df
+
+    def save_to_csv(self, df: pd.DataFrame, filename: str = None):
+        #if filename is None:
+        #    filename = f"{self.symbol}_daily_data.csv"
+        #df.to_csv(filename)
+        #print(f"✅ Saved {self.symbol} daily stock data to: {filename}")
+
+        if filename is None:
+            filename = f"{self.symbol}_daily_data.csv"
+
+        # Get the path to the parent directory of 'src'
+        base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        data_dir = os.path.join(base_dir, "data")
+
+        # Create 'data' directory if it doesn't exist
+        os.makedirs(data_dir, exist_ok=True)
+
+        # Full path to save the CSV
+        file_path = os.path.join(data_dir, filename)
+
+        df.to_csv(file_path)
+        print(f"✅ Saved {self.symbol} daily stock data to: {file_path}")
---
+++ src/main.py
@@ -0,0 +1,23 @@
+# src/main.py
+
+from data_loader import AlphaVantageDataLoader
+#from strategy import sma_crossover_strategy
+#from backtester import run_backtest
+#from metrics import compute_metrics
+#from alpha_vantage.timeseries import TimeSeries 
+#import requests
+#import pandas as pd
+
+# replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
+
+
+def main():
+    API_KEY = "BQCIW7GXBTH91G57"
+
+    AAPL_loader = AlphaVantageDataLoader(api_key=API_KEY, symbol="AAPL", outputsize="full")
+    AAPL_df = AAPL_loader.fetch_data()
+    AAPL_loader.save_to_csv(AAPL_df)
+    
+
+if __name__ == "__main__":
+    main()
